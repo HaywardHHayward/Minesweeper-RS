2d115369b2bd782d80c5fd389e3158d3e1ba991b
HaywardHHayward <tictanic8@hotmail.com>
2025-05-28 18:45:45 -0500

    Fuck it we ball, 5th rewrite of the GUI

--- src/gui.rs
+++ src/gui.rs
@@ -1,89 +1,12 @@
-use std::collections::HashMap;
-
 use iced::{Element, Task};
 
-pub mod main_menu;
-pub mod settings;
-#[derive(Debug, Clone, Copy)]
-pub enum Message {
-    MainMenu(main_menu::Message),
-    Settings(settings::Message),
-    ChangeScreen(Screen),
-}
-
-#[derive(PartialEq, Eq, Hash, Debug, Copy, Clone)]
-pub enum Screen {
-    MainMenu,
-    Settings,
-}
-
 #[derive(Debug)]
-pub enum ScreenState {
-    MainMenu(main_menu::MainMenu),
-    Settings(settings::Settings),
+pub enum Message {}
+
+pub fn update(state: &mut (), message: Message) -> Task<Message> {
+    Task::none()
 }
 
-impl ScreenState {
-    fn view(&self) -> Element<Message> {
-        match self {
-            ScreenState::MainMenu(main_menu) => main_menu.view().map(Message::MainMenu),
-            ScreenState::Settings(settings) => settings.view().map(Message::Settings),
-        }
-    }
-}
-
-#[derive(Debug)]
-pub struct Application {
-    current_screen: Screen,
-    screens: HashMap<Screen, ScreenState>,
-}
-
-impl Default for Application {
-    fn default() -> Self {
-        let mut screens = HashMap::with_capacity(1);
-        screens.insert(Screen::MainMenu, ScreenState::MainMenu(main_menu::MainMenu));
-        Self {
-            current_screen: Screen::MainMenu,
-            screens,
-        }
-    }
-}
-
-trait ScreenTrait: Into<ScreenState> {
-    type Message: std::fmt::Debug + Clone + Copy + Into<Message>;
-
-    fn update(&mut self, message: Self::Message) -> Task<Message>;
-    fn view(&self) -> Element<Self::Message>;
-}
-
-impl Application {
-    fn update(&mut self, message: Message) -> Task<Message> {
-        let screen = self
-            .screens
-            .get_mut(&self.current_screen)
-            .expect("Current screen should always exist");
-        match (screen, message) {
-            (_, Message::ChangeScreen(screen)) => {
-                self.current_screen = screen;
-                Task::none()
-            }
-            (ScreenState::MainMenu(main_menu), Message::MainMenu(msg)) => main_menu.update(msg),
-            (ScreenState::MainMenu(_), _) => Task::none(),
-            (ScreenState::Settings(settings), Message::Settings(msg)) => settings.update(msg),
-            (ScreenState::Settings(_), _) => Task::none(),
-        }
-    }
-    fn view(&self) -> Element<Message> {
-        self.screens
-            .get(&self.current_screen)
-            .expect("Current screen should always exist")
-            .view()
-    }
-}
-
-pub fn update(app: &mut Application, message: Message) -> Task<Message> {
-    app.update(message)
-}
-pub fn view(app: &Application) -> Element<Message> {
-    app.view()
+pub fn view(state: &()) -> Element<Message> {
+    iced::widget::text("Hello world!").into()
 }
