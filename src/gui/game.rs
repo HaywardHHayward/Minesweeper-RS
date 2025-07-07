c1d894bda217e5f0f7f62e9fc3821cdd584eaefc
HaywardHHayward <tictanic8@hotmail.com>
2025-07-07 11:18:21 -0500

    Refactor image module and update game selection handling; improve text input management

--- src/gui/game.rs
+++ src/gui/game.rs
@@ -73,7 +73,7 @@ impl ScreenTrait for Game {
     }
 }
 
-mod image_data {
+mod image_default {
     pub(super) static OPENED_CELL: &[u8] = include_bytes!("../../assets/default/OpenedCell.png");
     pub(super) static UNOPENED_CELL: &[u8] =
         include_bytes!("../../assets/default/UnopenedCell.png");
@@ -82,61 +82,61 @@ mod image_data {
 }
 
 impl Game {
-    fn cell(&self, x: u8, y: u8) -> Element<'_, Action> {
-        fn cell_view(cell: &crate::core::cell::Cell) -> Element<'_, Action> {
-            #[inline]
-            fn cell_container<'a>(element: impl Into<Element<'a, Action>>) -> Element<'a, Action> {
-                GuiWidget::container(element)
-                    .width(16)
-                    .height(16)
-                    .center(iced::Fill)
-                    .into()
-            }
-            let mut stack = GuiWidget::Stack::with_capacity(2).height(16).width(16);
-            if cell.is_open() {
-                let open_image =
-                    GuiImage::Image::new(GuiImage::Handle::from_bytes(image_data::OPENED_CELL));
-                stack = stack.push(open_image);
-                if cell.is_mine() {
-                    let mine_image = cell_container(GuiImage::Image::new(
-                        GuiImage::Handle::from_bytes(image_data::MINE),
-                    ));
-                    stack = stack.push(mine_image);
-                } else if let Some(adjacent_mines) = cell.adjacent_mines()
-                    && adjacent_mines > 0
-                {
-                    let color = match adjacent_mines {
-                        1 => iced::color!(0, 0, 255),
-                        2 => iced::color!(0, 127, 0),
-                        3 => iced::color!(255, 0, 0),
-                        4 => iced::color!(0, 0, 127),
-                        5 => iced::color!(127, 0, 0),
-                        6 => iced::color!(0, 127, 127),
-                        7 => iced::color!(255, 255, 255),
-                        8 => iced::color!(127, 127, 127),
-                        _ => std::unreachable!(),
-                    };
-                    let text = GuiWidget::text!("{adjacent_mines}")
-                        .font(iced::font::Font::MONOSPACE)
-                        .size(14)
-                        .color(color);
-                    stack = stack.push(cell_container(text));
-                }
-            } else {
-                let unopened_image =
-                    GuiImage::Image::new(GuiImage::Handle::from_bytes(image_data::UNOPENED_CELL));
-                stack = stack.push(unopened_image);
-                if cell.is_flagged() {
-                    let flag_image = cell_container(GuiImage::Image::new(
-                        GuiImage::Handle::from_bytes(image_data::FLAG),
-                    ));
-                    stack = stack.push(flag_image);
-                }
-            }
-            stack.width(16).height(16).into()
+    fn cell_view(cell: &crate::core::cell::Cell) -> Element<'_, Action> {
+        #[inline]
+        fn cell_container<'a>(element: impl Into<Element<'a, Action>>) -> Element<'a, Action> {
+            GuiWidget::container(element)
+                .width(16)
+                .height(16)
+                .center(iced::Fill)
+                .into()
         }
+        let mut stack = GuiWidget::Stack::with_capacity(2).height(16).width(16);
+        if cell.is_open() {
+            let open_image =
+                GuiImage::Image::new(GuiImage::Handle::from_bytes(image_default::OPENED_CELL));
+            stack = stack.push(open_image);
+            if cell.is_mine() {
+                let mine_image = cell_container(GuiImage::Image::new(
+                    GuiImage::Handle::from_bytes(image_default::MINE),
+                ));
+                stack = stack.push(mine_image);
+            } else if let Some(adjacent_mines) = cell.adjacent_mines()
+                && adjacent_mines > 0
+            {
+                let color = match adjacent_mines {
+                    1 => iced::color!(0, 0, 255),
+                    2 => iced::color!(0, 127, 0),
+                    3 => iced::color!(255, 0, 0),
+                    4 => iced::color!(0, 0, 127),
+                    5 => iced::color!(127, 0, 0),
+                    6 => iced::color!(0, 127, 127),
+                    7 => iced::color!(255, 255, 255),
+                    8 => iced::color!(127, 127, 127),
+                    _ => std::unreachable!(),
+                };
+                let text = GuiWidget::text!("{adjacent_mines}")
+                    .font(iced::font::Font::MONOSPACE)
+                    .size(14)
+                    .color(color);
+                stack = stack.push(cell_container(text));
+            }
+        } else {
+            let unopened_image =
+                GuiImage::Image::new(GuiImage::Handle::from_bytes(image_default::UNOPENED_CELL));
+            stack = stack.push(unopened_image);
+            if cell.is_flagged() {
+                let flag_image = cell_container(GuiImage::Image::new(
+                    GuiImage::Handle::from_bytes(image_default::FLAG),
+                ));
+                stack = stack.push(flag_image);
+            }
+        }
+        stack.width(16).height(16).into()
+    }
+    fn cell(&self, x: u8, y: u8) -> Element<'_, Action> {
         let cell = self.board.get_cell(x, y).expect("Cell should exist");
-        let cell_view = cell_view(cell);
+        let cell_view = Game::cell_view(cell);
         GuiWidget::mouse_area(cell_view)
             .on_press(Action::OpenCell(x, y))
             .on_right_press(Action::ToggleFlag(x, y))
--- src/gui/game_selection.rs
+++ src/gui/game_selection.rs
@@ -1,8 +1,6 @@
-﻿use std::num::{NonZeroU8, NonZeroU16};
+﻿use iced::{Element, Task, widget as GuiWidget};
 
-use iced::Task;
-
-use crate::gui::{Message, ScreenTrait};
+use crate::gui::{Message as AppMessage, ScreenMessage, ScreenTrait};
 
 #[derive(Debug, Default)]
 pub struct GameSelection {
@@ -18,9 +16,16 @@ enum GameSelectionImpl {
 
 #[derive(Debug, Default)]
 struct CustomSelection {
-    row: Option<NonZeroU8>,
-    column: Option<NonZeroU8>,
-    mines: Option<NonZeroU16>,
+    row: String,
+    column: String,
+    mines: String,
+}
+
+#[derive(Debug, Clone)]
+enum TextChangedEnum {
+    Row,
+    Column,
+    Mines,
 }
 
 #[derive(Debug, Clone)]
@@ -30,12 +35,13 @@ pub enum Action {
     GoToOptionSelection,
     CheckCustom,
     ReturnToMainMenu,
+    TextChanged(TextChangedEnum, String),
 }
 
 impl ScreenTrait for GameSelection {
     type Message = Action;
 
-    fn update(&mut self, message: Self::Message) -> Task<Message> {
+    fn update(&mut self, message: Self::Message) -> Task<AppMessage> {
         match message {
             Action::StartGame(options) => {
                 let board = match options {
@@ -44,11 +50,13 @@ impl ScreenTrait for GameSelection {
                     Options::Expert => crate::core::board::Board::create_expert(),
                     Options::Custom => {
                         // Short-circuit, go to custom board screen for setup
-                        return Task::none();
+                        return Task::done(AppMessage::ScreenAction(ScreenMessage::GameSelection(
+                            Action::GoToCustom,
+                        )));
                     }
                 };
                 let game = crate::gui::game::Game::new(board);
-                Task::done(Message::InitializeScreen {
+                Task::done(AppMessage::InitializeScreen {
                     screen_type: crate::gui::ScreenType::Game,
                     initializer_fn: Box::new(|| crate::gui::Screen::Game(game)),
                     change_screen: true,
@@ -69,7 +77,105 @@ impl ScreenTrait for GameSelection {
                 Task::none()
             }
             Action::ReturnToMainMenu => {
-                Task::done(Message::ChangeScreen(crate::gui::ScreenType::MainMenu))
+                Task::done(AppMessage::ChangeScreen(crate::gui::ScreenType::MainMenu))
+            }
+            Action::TextChanged(selection, string) => {
+                let GameSelectionImpl::CustomSelection(CustomSelection {
+                    ref mut row,
+                    ref mut column,
+                    ref mut mines,
+                }) = self.state
+                else {
+                    return Task::none();
+                };
+                let numeric_string = string
+                    .matches(|ref char| char::is_ascii_digit(char))
+                    .collect::<String>();
+                match selection {
+                    TextChangedEnum::Row => {
+                        if numeric_string.len() <= 2 {
+                            *row = numeric_string;
+                        }
+                    }
+                    TextChangedEnum::Column => {
+                        if numeric_string.len() <= 2 {
+                            *column = numeric_string;
+                        }
+                    }
+                    TextChangedEnum::Mines => {
+                        if numeric_string.len() <= 4 {
+                            *mines = numeric_string;
+                        }
+                    }
+                }
+                Task::none()
+            }
+        }
+    }
+
+    fn view(&self) -> Element<'_, Self::Message> {
+        match self.state {
+            GameSelectionImpl::OptionSelection => {
+                let beginner = GuiWidget::button("Beginner")
+                    .on_press(Action::StartGame(Options::Beginner))
+                    .width(iced::Fill);
+                let intermediate = GuiWidget::button("Intermediate")
+                    .on_press(Action::StartGame(Options::Intermediate))
+                    .width(iced::Fill);
+                let expert = GuiWidget::button("Expert")
+                    .on_press(Action::StartGame(Options::Expert))
+                    .width(iced::Fill);
+                let custom = GuiWidget::button("Custom")
+                    .on_press(Action::StartGame(Options::Custom))
+                    .width(iced::Fill);
+                let options = GuiWidget::column![beginner, intermediate, expert, custom].width(110);
+                let return_to_menu =
+                    GuiWidget::button("Return to main menu").on_press(Action::ReturnToMainMenu);
+                let content = GuiWidget::column![options, return_to_menu].spacing(20);
+                GuiWidget::container(content).center(iced::Fill).into()
+            }
+            GameSelectionImpl::CustomSelection(ref custom) => {
+                const EDITOR_WIDTH: f32 = 60.0;
+                let row_text = GuiWidget::text!("Rows: ");
+                let row_editor = GuiWidget::text_input("", &custom.row)
+                    .on_input(|input| Action::TextChanged(TextChangedEnum::Row, input))
+                    .width(iced::Fill);
+                let column_text = GuiWidget::text("Columns: ");
+                let column_editor = GuiWidget::text_input("", &custom.column)
+                    .on_input(|input| Action::TextChanged(TextChangedEnum::Column, input))
+                    .width(iced::Fill);
+                let mines_text = GuiWidget::text("Mines: ");
+                let mines_editor = GuiWidget::text_input("", &custom.mines)
+                    .on_input(|input| Action::TextChanged(TextChangedEnum::Mines, input))
+                    .width(iced::Fill);
+                let texts = GuiWidget::column![row_text, column_text, mines_text]
+                    .spacing(10)
+                    .align_x(iced::Left);
+                let editors = GuiWidget::column![row_editor, column_editor, mines_editor]
+                    .width(EDITOR_WIDTH)
+                    .align_x(iced::Right);
+                let custom_editors = GuiWidget::row![texts, editors]
+                    .height(iced::Shrink)
+                    .align_y(iced::Center);
+                let create_button = GuiWidget::button("Create board").on_press_maybe(
+                    if !custom.row.is_empty()
+                        && !custom.column.is_empty()
+                        && !custom.mines.is_empty()
+                    {
+                        Some(Action::CheckCustom)
+                    } else {
+                        None
+                    },
+                );
+                let custom_content = GuiWidget::column![custom_editors, create_button]
+                    .align_x(iced::Center)
+                    .spacing(10);
+                let return_button =
+                    GuiWidget::button("Return to options").on_press(Action::GoToOptionSelection);
+                let content = GuiWidget::column![custom_content, return_button]
+                    .align_x(iced::Center)
+                    .spacing(20);
+                GuiWidget::container(content).center(iced::Fill).into()
             }
         }
     }
