0d0ab1d9cdc9ebb8e35a4c4b2c8b99ed292cf892
HaywardHHayward <tictanic8@hotmail.com>
2025-08-25 18:07:19 -0500

    Create backbone of Game, in preparation for implementing the game proper

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 pub mod core;
 mod gui;
-pub use core::{board::Board, cell::Cell};
+pub use core::{board::*, cell::*};
 
 pub use gui::{
-    Application, ArcLock, Screen, config::Config, scale_factor, subscription, theme, update, view,
+    Application, ArcLock, Screen, config::*, scale_factor, subscription, theme, update, view,
 };
