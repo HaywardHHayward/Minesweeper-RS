56d0c14054a7a29b0865898770747747186be4d9
HaywardHHayward <tictanic8@hotmail.com>
2025-09-11 11:39:47 -0500

    Add application id

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ use minesweeper_rs::*;
 pub fn main() -> iced::Result {
     iced::application(Application::create, update, view)
         .title("Minesweeper")
-        .settings(iced::Settings::default())
+        .settings(iced::Settings {
+            id: Some("com.github.haywardhhayward.Minesweeper".to_string()),
+            ..Default::default()
+        })
         .window(iced::window::Settings {
             icon: iced::window::icon::from_file_data(assets::ICON, None).ok(),
             exit_on_close_request: false,
